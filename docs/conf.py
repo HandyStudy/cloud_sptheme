111689e3aaee80b86bf69a9ba1b1416a12c8d570
Eli Collins <elic@assurancetechnologies.com>
2011-10-10 00:25:46 -0400

    added ability to collapse sidebar, controlled by small button in upper right corner.
    also, sidebarwidth now configurable

--- docs/conf.py
+++ docs/conf.py
@@ -114,7 +114,7 @@ html_theme = 'cloud'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = { "roottarget": index_doc }
+html_theme_options = { "roottarget": index_doc, "collapsiblesidebar": True }
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [csp.get_theme_dir()]
